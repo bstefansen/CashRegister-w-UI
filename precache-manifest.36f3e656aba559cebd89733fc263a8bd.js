b40717d62bd1c8962c6f7088fd900f150ff6e53b
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-01-10 22:13:13 -0500

    Updates

---
+++ precache-manifest.36f3e656aba559cebd89733fc263a8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5878c903772859b24fc1ca037a1acbb7",
+    "url": "/CashRegister-w-UI/index.html"
+  },
+  {
+    "revision": "30c8800dd083a748fa6d",
+    "url": "/CashRegister-w-UI/static/css/main.5197ceae.chunk.css"
+  },
+  {
+    "revision": "6277bb431a24e7e6041d",
+    "url": "/CashRegister-w-UI/static/js/2.585d8809.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CashRegister-w-UI/static/js/2.585d8809.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30c8800dd083a748fa6d",
+    "url": "/CashRegister-w-UI/static/js/main.531fc089.chunk.js"
+  },
+  {
+    "revision": "d982d06b45ca0ef4b265",
+    "url": "/CashRegister-w-UI/static/js/runtime-main.9630a35a.js"
+  }
+]);
